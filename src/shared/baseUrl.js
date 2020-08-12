f0f29e6b9d7f3918c79d42bc1a833ddb28410324
bermarte <bernardo.martelli@gmail.com>
2020-08-13 01:52:46 +0200

    changing baseUrl from localhost to heroku

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,3 @@
 /* configuration file for the server */
-export const baseUrl = 'http://localhost:3001/';
+//export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = 'https://react-confusion-bermarte.herokuapp.com/';
